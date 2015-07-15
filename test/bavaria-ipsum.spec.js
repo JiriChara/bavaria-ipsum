3c095663f783c6c46e1dfa48fd003fe2bb1d7916
Jiri Chara <jirik.chara@gmail.com>
2015-07-15 20:25:07 +0200

    Add more specs

--- test/bavaria-ipsum.spec.js
+++ test/bavaria-ipsum.spec.js
@@ -82,22 +82,138 @@ describe('BavariaIpsum', function () {
         });
     });
 
-    describe('generateWord method', function () {
+    describe('instance methods', function () {
         beforeEach(function () {
             this.bavariaIpsum = new this.BavariaIpsum();
         });
 
-        it('returns random word from source array', function () {
-            spyOn(this.bavariaIpsum, '_getRandomInt').and.returnValue(10);
+        describe('generateWord', function () {
+            it('returns random word from source array', function () {
+                spyOn(this.bavariaIpsum, '_getRandomInt').and.returnValue(10);
 
-            expect(this.bavariaIpsum.generateWord()).toBe(
-                this.bavariaIpsum.source[10]
-            );
+                expect(this.bavariaIpsum.generateWord()).toBe(
+                    this.bavariaIpsum.source[10]
+                );
 
-            expect(this.bavariaIpsum._getRandomInt).toHaveBeenCalledWith(
-                0,
-                this.bavariaIpsum.source.length
-            );
+                expect(this.bavariaIpsum._getRandomInt).toHaveBeenCalledWith(
+                    0,
+                    this.bavariaIpsum.source.length
+                );
+            });
+        });
+
+        describe('generateSentence', function () {
+            it('generates sentence', function () {
+                expect(this.bavariaIpsum.generateSentence()).toBeString();
+            });
+
+            it('generates sentence of given length', function () {
+                var i = 500;
+
+                while (i-- > 0) {
+                    expect(this.bavariaIpsum.generateSentence(1)).toMatch(
+                        /^.+[.?!]$/i
+                    );
+
+                    expect(this.bavariaIpsum.generateSentence(2)).toMatch(
+                        /^.+(\s|,)+.+[.?!]$/i
+                    );
+                }
+            });
+
+            it('starts with capitalized letter', function () {
+                var i = 500,
+                    firstLetter;
+
+                while (i-- > 0) {
+                    firstLetter = this.bavariaIpsum.generateSentence()[0];
+                    expect(firstLetter).toEqual(
+                        firstLetter.toUpperCase()
+                    );
+                }
+            });
+
+            it('ends with ? . or !', function () {
+                var i = 500,
+                    sentence,
+                lastLetter;
+
+                while (i-- > 0) {
+                    sentence = this.bavariaIpsum.generateSentence();
+                    lastLetter = sentence[sentence.length - 1];
+
+                    expect(lastLetter).toMatch(
+                        /^[?.!]$/
+                    );
+                }
+            });
+
+            it('adds comma', function () {
+                this.bavariaIpsum.showCommaChance = 1; // 100%
+
+                var i = 500;
+
+                while (i-- > 0) {
+                    expect(this.bavariaIpsum.generateSentence(2)).toMatch(
+                        /^.+,\s.+[.?!]$/i
+                    );
+
+                    expect(
+                        this.bavariaIpsum.generateSentence(2).indexOf(',') > 0
+                    ).toBe(true);
+                }
+            });
+        });
+
+        describe('generateParagraph', function () {
+            it('returns string', function () {
+                expect(this.bavariaIpsum.generateParagraph()).toBeString();
+            });
+
+            it('generates given count of sentences', function () {
+                var i = 500;
+
+                while (i-- > 0) {
+                    expect(this.bavariaIpsum.generateParagraph(1)).toMatch(
+                        /^.+[.?!]$/
+                    );
+
+                    expect(this.bavariaIpsum.generateParagraph(2)).toMatch(
+                        /^.+[.?!]\s.+[.?!]$/
+                    );
+                }
+            });
+
+            it('starts with startingSentence if option given', function () {
+                var i = 500;
+
+                while (i-- > 0) {
+                    expect(this.bavariaIpsum.generateParagraph({
+                        useStartingSentence: true
+                    }).indexOf(this.bavariaIpsum.startSentence))
+                        .toEqual(0);
+                }
+            });
+
+            it('throws an error when length is invalid', function () {
+                var self = this;
+
+                expect(function () {
+                    self.bavariaIpsum.generateParagraph('foo');
+                }).toThrowError('Length must be an Integer');
+
+                expect(function () {
+                    self.bavariaIpsum.generateParagraph(5.5);
+                }).toThrowError('Length must be an Integer');
+            });
+
+            it('throws an error when opts is invalid', function () {
+                var self = this;
+
+                expect(function () {
+                    self.bavariaIpsum.generateParagraph(1, 1);
+                }).toThrowError('Opts must be an Object');
+            });
         });
     });
 });
